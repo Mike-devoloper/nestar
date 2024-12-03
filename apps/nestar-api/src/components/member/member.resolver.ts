92bd275c71c97eb9e92a3842cea6e644530a65f9
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2024-12-03 22:36:14 +0900

    fix: integrate global pipe validation

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -9,7 +9,6 @@ export class MemberResolver {
     constructor(private readonly memberservice: MemberService){}
 
     @Mutation(() => Member)
-    @UsePipes(ValidationPipe)
     public async signup(@Args("input") input:MemberInput ):Promise<Member> {
         try {
             console.log("Mutatiion: signup")
@@ -21,7 +20,6 @@ export class MemberResolver {
         }
     }
     @Mutation(() => Member)
-    @UsePipes(ValidationPipe)
     public async  login(@Args("input") input:LoginInput):Promise<Member> {
         try {
             console.log("Mutatiion: login")
